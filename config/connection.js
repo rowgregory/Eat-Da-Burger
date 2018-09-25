8576963921d986e75a58c24fd4d0e2f6cf52255d
row_gregory <rowgregory@gmail.com>
2018-09-24 20:30:32 -0400

    added Jaws

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,17 @@ var connection = mysql.createConnection({
   password: "SuperSecretPasswordHere",
   database: "burgers_db"
 });
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "hacktheplanet",
+    database: "burgers_db"
+  });
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
