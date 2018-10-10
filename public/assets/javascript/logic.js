3b9ccfc68b05445d05cdb12fb884715f606aac1d
row_gregory <rowgregory@gmail.com>
2018-10-09 21:27:48 -0400

    added bootstrap

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -1,6 +1,17 @@
-$('#eatMe').on('click', function() {
-    event.preventDefault();
+// $('#eatMe').on('click', function() {
+//     event.preventDefault();
 
-    let burger_name = $('#newB').val().trim();
-    console.log(burger_name);
+//     let burger_name = $('#newB').val().trim();
+//     console.log(burger_name);
+// })
+
+$('.submitCoaster').on('click', function() {
+    
+    event.preventDefault();
+    var input = $('#newB').val().trim();
+    if (!input) {
+        alert('Please create coaster!');
+    }
+    
 })
+
