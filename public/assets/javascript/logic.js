2266c9271ba77a5dcb53c7be157464133d6188fd
row_gregory <rowgregory@gmail.com>
2018-10-13 20:10:18 -0400

    tooltip

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -26,3 +26,20 @@ $(function () {
     })
  
 
+    $(document).ready(function(){
+        $("#toptip").tooltip({
+            placement:"top"
+        });
+
+        $("#bottomtip").tooltip({
+            placement:"bottom"
+        });
+
+        $("#lefttip").tooltip({
+            placement:"left"
+        });
+
+        $("#righttip").tooltip({
+            placement:"right"
+        });                
+    });
